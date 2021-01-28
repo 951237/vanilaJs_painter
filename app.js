72d0e94f8d26dfa69cd57ae88428e49e2434a2b6
951237 <951237.gmail.com>
2021-01-29 02:36:39 +0900

    Doc : Update - 캔버스에 기본 선 그리기 - 캔버스 크기 설정하기 - 캔버스 cxt 라인 색, 두깨 설정 - 기본 선 그리기

--- app.js
+++ app.js
@@ -1,5 +1,12 @@
-const canvas = document.getElementById("jsCanvas");
+const canvas = document.getElementById("jsCanvas"); // html에서 캔버스 개체 가져오기
+const ctx = canvas.getContext("2d"); // canvas 
 
+// 그림을 그릴수 있는 캔버스 크기를 js에서도 설정
+canvas.width = 700;
+canvas.height = 700;
+
+ctx.strokStyle = "#2c2c2c"; // 선의 기본 색깔 검은색
+ctx.lineWidth = 2.5; // 선의 굵기 지정
 
 let painting = false; //그리기 모드 중지
 
@@ -12,6 +19,20 @@ function stopPainting() {
 function onMouseMove(event) {
     const x = event.offsetX;
     const y = event.offsetY;
+    if (!painting) { // 그리기모드가 아닐때
+        console.log("creating path in ", x, y);
+        ctx.beginPath(); // 움질일때 패스가 만들어짐
+        ctx.moveTo(x, y); // 좌표를 움직임
+    } else { // 그리기 모드가 활성화 될때
+        console.log("creating line in ", x, y);
+        ctx.lineTo(x, y);
+        ctx.stroke();
+    }
+}
+
+// 그림그리기 시작
+function startPainting() {
+    painting = true;
 }
 
 // 캔버스에 마우스를 클릭 했을 때
@@ -20,15 +41,9 @@ function onMouseDown(event) {
     // console.log(event)
 }
 
-// 캔버스에서 마우스를 떼었을 때 함수
-function onMouseUp(event) {
-    stopPainting(); // 그리기 모드 비활성화
-}
-
-
 if (canvas) {
     canvas.addEventListener("mousemove", onMouseMove);
     canvas.addEventListener("mousedown", onMouseDown);
-    canvas.addEventListener("mouseup", onMouseUp);
+    canvas.addEventListener("mouseup", stopPainting);
     canvas.addEventListener("mouseleave", stopPainting);
 }
