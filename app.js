363380df7a5e830d527587de29df1f67207a52e6
951237 <951237.gmail.com>
2021-01-29 01:57:14 +0900

    Doc : Create - 캔버스에서의 기본 움직임 함수 작성 - 캔버스 위에 마우스가 올라있을 때 - 캔버스 위에 마우스를 클릭했을 때 - 캔버스 위에서 마우스를 떼었을 때 - 캔버스 위에서 마우스가 떠났을 때

--- app.js
+++ app.js
@@ -1,1 +1,34 @@
-/* 
+const canvas = document.getElementById("jsCanvas");
+
+
+let painting = false; //그리기 모드 중지
+
+// 그리기 모드 중지
+function stopPainting() {
+    painting = false;
+}
+
+// 캔버스에 마우스가 움직일때 좌표값을 반환
+function onMouseMove(event) {
+    const x = event.offsetX;
+    const y = event.offsetY;
+}
+
+// 캔버스에 마우스를 클릭 했을 때
+function onMouseDown(event) {
+    painting = true; // 그리기 모드 활성화
+    // console.log(event)
+}
+
+// 캔버스에서 마우스를 떼었을 때 함수
+function onMouseUp(event) {
+    stopPainting(); // 그리기 모드 비활성화
+}
+
+
+if (canvas) {
+    canvas.addEventListener("mousemove", onMouseMove);
+    canvas.addEventListener("mousedown", onMouseDown);
+    canvas.addEventListener("mouseup", onMouseUp);
+    canvas.addEventListener("mouseleave", stopPainting);
+}
